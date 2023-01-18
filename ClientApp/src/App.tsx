66bc7a819ce4f07891414332ffb4c727c530e8e8
Tricia Amheiser <tricia@taarts.net>
2023-01-18 15:01:37 -0500

    added raw.json for data tried to set up main.tsx

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <div>
+      <body className="wrapper">
+        <header>
+          <nav></nav>
+        </header>
+        <p>welcome</p>
+        <h1>Lorem ipsum dolor</h1>
+        <h3>everything in it's right place</h3>
+        <p>start</p>
+      </body>
+    </div>
+  )
 }
--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
 import { App } from './App'
 import './index.scss'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>
 )
